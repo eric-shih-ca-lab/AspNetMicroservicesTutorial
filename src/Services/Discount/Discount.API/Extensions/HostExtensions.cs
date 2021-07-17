78ca8c51860b682d8db72957e0811bf45af147e3
Eric.Shih <Eric.Shih@advantech.com>
2021-07-17 04:45:44 -0700

    fix postgresql connection not open bug and re-engineering migration

--- src/Services/Discount/Discount.API/Extensions/HostExtensions.cs
+++ src/Services/Discount/Discount.API/Extensions/HostExtensions.cs
@@ -21,25 +21,7 @@ namespace Discount.API.Extensions
                 try
                 {
                     logger.LogInformation("Migrating postresql databases.");
-                    using var connection = new NpgsqlConnection(configuration.GetValue<string>("DatabaseSettings:ConnectionString"));
-                    using var command = new NpgsqlCommand
-                    {
-                        Connection = connection
-                    };
-                    command.CommandText = "DROP TABLE IF EXISTS Coupon";
-                    command.ExecuteNonQuery();
-
-                    command.CommandText = @"CREATE TABLE Coupon(Id SERIAL PRIMARY KEY,
-                                                                ProductName VARCHAR(24) NOT NULL,
-                                                                Description TEXT,
-                                                                Amount INT)";
-                    command.ExecuteNonQuery();
-
-                    command.CommandText = "INSERT INTO Coupon(ProductName, Description, Amount) VALUES('IPhone X', 'IPhone Discount', 150)";
-                    command.ExecuteNonQuery();
-
-                    command.CommandText = "INSERT INTO Coupon(ProductName, Description, Amount) VALUES('Samsung 10', 'Sumsung Discount', 100)";
-                    command.ExecuteNonQuery();
+                    ExecutingMigration(configuration);
 
                     logger.LogInformation("Migrated postresql databases.");
                 }
@@ -56,6 +38,31 @@ namespace Discount.API.Extensions
             }
 
             return host;
+        }
+
+        private static void ExecutingMigration(IConfiguration configuration)
+        {
+            using var connection = new NpgsqlConnection(configuration.GetValue<string>("DatabaseSettings:ConnectionString"));
+            connection.Open();
+
+            using var command = new NpgsqlCommand
+            {
+                Connection = connection
+            };
+            command.CommandText = "DROP TABLE IF EXISTS Coupon";
+            command.ExecuteNonQuery();
+
+            command.CommandText = @"CREATE TABLE Coupon(Id SERIAL PRIMARY KEY,
+                                                                ProductName VARCHAR(24) NOT NULL,
+                                                                Description TEXT,
+                                                                Amount INT)";
+            command.ExecuteNonQuery();
+
+            command.CommandText = "INSERT INTO Coupon(ProductName, Description, Amount) VALUES('IPhone X', 'IPhone Discount', 150)";
+            command.ExecuteNonQuery();
+
+            command.CommandText = "INSERT INTO Coupon(ProductName, Description, Amount) VALUES('Samsung 10', 'Sumsung Discount', 100)";
+            command.ExecuteNonQuery();
         }
     }
 }
--- src/Services/Discount/Discount.API/readme.txt
+++ src/Services/Discount/Discount.API/readme.txt
@@ -9,3 +9,5 @@ docker pull dpage/pgadmin4
 --Nuget Packages
 Npgsql
 Dapper
+
+docker-compose -f docker-compose.yml -f docker-compose.override.yml up -d
